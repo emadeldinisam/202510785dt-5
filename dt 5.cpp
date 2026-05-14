37e5e17237f69063f25c1fea545280a7a69e24b0
emadeldinisam <madsam832@gmail.com>
2026-05-14 04:42:03 -0700

    Add files via upload

---
+++ dt 5.cpp
@@ -0,0 +1,8 @@
+void greet(string name) {    // takes a parameter
+    cout << "Hello " << name << endl;  // but returns nothing
+}
+
+int main() {
+    greet("Ahmed");   // output: Hello Ahmed
+    return 0;
+}
